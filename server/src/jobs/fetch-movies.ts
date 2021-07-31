e4cc15f56922c0dba35cde4a043ed6bb9f9f7429
toan.fedev <toan.fedev@hitak.net>
2021-07-31 11:11:07 +0700

    [be] - fix aggregation, comment cronjob

--- server/src/jobs/fetch-movies.ts
+++ server/src/jobs/fetch-movies.ts
@@ -2,15 +2,17 @@ const CronJob = require('cron').CronJob;
 import { Movie } from '../models/movie';
 import axios from 'axios';
 
+// const job = new CronJob('* * 0 * * *', async function () {
 const job = new CronJob('*/10 * * * * *', async function () {
     try {
-        const { data: movies } = await axios.get(
-            `${process.env.DATA_URI}/discover/movie`,
-            {
-                params: { api_key: process.env.DATA_KEY },
-            }
-        );
-        console.log(movies.results.length);
+        // const { data: movies } = await axios.get(
+        //     `${process.env.DATA_URI}/discover/movie`,
+        //     {
+        //         params: { api_key: process.env.DATA_KEY },
+        //     }
+        // );
+        // console.log(movies.results.length);
+        console.log('Test cronjob...');
     } catch (e) {
         console.error(e);
     }
--- server/src/services/movie.ts
+++ server/src/services/movie.ts
@@ -9,8 +9,28 @@ const instance = {
             limit: 20,
         };
         try {
+            let aggregate = Movie.aggregate();
+            aggregate.sort({ popularity: -1 });
+            aggregate.project({
+                _id: 0,
+                id: 1,
+                adult: 1,
+                backdrop_path: 1,
+                genres: 1,
+                original_language: 1,
+                original_title: 1,
+                overview: 1,
+                popularity: 1,
+                poster_path: 1,
+                release_date: 1,
+                title: 1,
+                video: 1,
+                vote_average: 1,
+                vote_count: 1,
+            });
+
             // @ts-ignore
-            const movies = await Movie.aggregatePaginate({}, options);
+            const movies = await Movie.aggregatePaginate(aggregate, options);
             return movies;
         } catch (e) {
             throw new NotFoundError();
